49f8c904fc8d568b63ad0d4e1f1a4703e8d34bcc
Fumiko Toyoda <toyodafumiko@gmail.com>
2023-11-11 09:42:46 +0900

    add hand drwoing fonts

--- js/app.js
+++ js/app.js
@@ -99,13 +99,13 @@ function appendMemo({ title, text, timestamp, image }) {
     const pageElement = document.createElement('label');
     pageElement.innerHTML = `
         <input type="checkbox" />
-        <span style="z-index: 95;">
+        <span class="page-dontents" style="z-index: 95;">
             <p>${dateStr}</p>
             <p>${title}</p>
             <img src="${image}" alt="${title}">
             <p>${text}</p>
         </span>
-        <span class="dummy">
+        <span class="page-dontents" class="dummy">
             <p>${dateStr}</p>
             <p>${title}</p>
             <img src="${image}" alt="${title}">
